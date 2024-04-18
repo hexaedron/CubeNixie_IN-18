35bac6001aa038b37adb3758b81fadd416e6ac68
Konstantin Gorbenko <kyb.6.granei@yandex.ru>
2024-04-18 22:32:15 +0300

    Сделал заполнение массива байт, отправляемого в регистры.

--- populateIN18.cpp
+++ populateIN18.cpp
@@ -4,6 +4,11 @@
 // в 10 битные посадочные места.
 void populateIN18(char* datetime, byte* shiftBytes)
 {
+  shiftBytes[0] = 0;
+  shiftBytes[1] = 0;
+  shiftBytes[2] = 0;
+  shiftBytes[3] = 0;
+  shiftBytes[4] = 0;
   
   //// Обработка 2 байта segBytes
   //// Фиксим ошибку в плате - инвертированные ножки у U7
@@ -16,132 +21,172 @@ void populateIN18(char* datetime, byte* shiftBytes)
   switch(datetime[0])
   {
     case '0':
+      bitSet(shiftBytes[0], 0);
     break;
 
     case '1':
+      bitSet(shiftBytes[0], 1);
     break;
 
     case '2':
+      bitSet(shiftBytes[0], 2);
     break;
 
     case '3':
+      bitSet(shiftBytes[0], 3);
     break;
 
     case '4':
+      bitSet(shiftBytes[0], 4);
     break;
 
     case '5':
+      bitSet(shiftBytes[0], 5);
     break;
 
     case '6':
+      bitSet(shiftBytes[0], 6);
     break;
 
     case '7':
+      bitSet(shiftBytes[0], 7);
     break;
 
     case '8':
+      bitSet(shiftBytes[1], 0);
     break;
 
     case '9':
+      bitSet(shiftBytes[1], 1);
     break;
   }
 
   switch(datetime[1])
   {
     case '0':
+      bitSet(shiftBytes[1], 2);
     break;
 
     case '1':
+      bitSet(shiftBytes[1], 3);
     break;
 
     case '2':
+      bitSet(shiftBytes[1], 4);
     break;
 
     case '3':
+     bitSet(shiftBytes[1], 5);
     break;
 
     case '4':
+      bitSet(shiftBytes[1], 6);
     break;
 
     case '5':
+      bitSet(shiftBytes[1], 7);
     break;
 
     case '6':
+      bitSet(shiftBytes[2], 0);
     break;
 
     case '7':
+      bitSet(shiftBytes[2], 1);
     break;
 
     case '8':
+      bitSet(shiftBytes[2], 2);
     break;
 
     case '9':
+      bitSet(shiftBytes[2], 3);
     break;
   }
 
   switch(datetime[2])
   {
     case '0':
+      bitSet(shiftBytes[2], 4);
     break;
 
     case '1':
+      bitSet(shiftBytes[2], 5);
     break;
 
     case '2':
+      bitSet(shiftBytes[2], 6);
     break;
 
     case '3':
+      bitSet(shiftBytes[2], 7);
     break;
 
     case '4':
+      bitSet(shiftBytes[3], 0);
     break;
 
     case '5':
+      bitSet(shiftBytes[3], 1);
     break;
 
     case '6':
+      bitSet(shiftBytes[3], 2);
     break;
 
     case '7':
+      bitSet(shiftBytes[3], 3);
     break;
 
     case '8':
+      bitSet(shiftBytes[3], 4);
     break;
 
     case '9':
+      bitSet(shiftBytes[3], 5);
     break;
   }
 
   switch(datetime[3])
   {
     case '0':
+      bitSet(shiftBytes[3], 6);
     break;
 
     case '1':
+      bitSet(shiftBytes[3], 7);
     break;
 
     case '2':
+      bitSet(shiftBytes[4], 0);
     break;
 
     case '3':
+      bitSet(shiftBytes[4], 1);
     break;
 
     case '4':
+      bitSet(shiftBytes[4], 2);
     break;
 
     case '5':
+      bitSet(shiftBytes[4], 3);
     break;
 
     case '6':
+      bitSet(shiftBytes[4], 4);
     break;
 
     case '7':
+      bitSet(shiftBytes[4], 5);
     break;
 
     case '8':
+      bitSet(shiftBytes[4], 6);
     break;
 
     case '9':
+      bitSet(shiftBytes[4], 7);
     break;
   }
 }
